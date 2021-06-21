f2c3b14373040692f4e52586542b5971367a5052
Connor <Connor.Jones194@btinternet.com>
2021-06-21 15:01:11 +0100

    update

--- pet-charm/js.js
+++ pet-charm/js.js
@@ -1,18 +1,14 @@
-function calculate(){
-    var days = new Date();
-    days = new Date(2021,6,days.getDate());
-    var endDate = new Date(2021,6,30);
-    var daysLeft = (endDate.getTime() - days.getTime())/86400000;
-
-    var charms = $("#input").val();
-    var needed = 10000 - charms;
-    var daily = needed / daysLeft;
-
-    document.getElementById("needed").innerHTML = "You need : " + daily + " charms a day :)";
-
-};
 $(document).ready(function(){
     $("#input").on("input", function(){
-        calculate();
+        var days = new Date();
+        days = new Date(2021,6,days.getDate());
+        var endDate = new Date(2021,6,30);
+        var daysLeft = (endDate.getTime() - days.getTime())/86400000;
+    
+        var charms = $("#input").val();
+        var needed = 10000 - charms;
+        var daily = Math.round(needed / daysLeft);
+    
+        document.getElementById("needed").innerHTML = "You need " + daily + " charms a day :)";
     });
 });
