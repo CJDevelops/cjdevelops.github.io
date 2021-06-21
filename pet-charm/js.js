ecef2b5f9ab5622e90b4b52c890051332c130372
Connor <Connor.Jones194@btinternet.com>
2021-06-21 11:56:05 +0100

    added charms

---
+++ pet-charm/js.js
@@ -0,0 +1,13 @@
+function calculate(){
+    var days = new Date();
+    days = new Date(2021,6,days.getDate());
+    var endDate = new Date(2021,6,30);
+    var daysLeft = (endDate.getTime() - days.getTime())/86400000;
+
+    var charms = $("#input").val();
+    var needed = 10000 - charms;
+    var daily = needed / daysLeft;
+
+    document.getElementById("needed").innerHTML = "You need : " + daily + " charms a day :)";
+
+};
